df4ae0d684341c79cf350e6e14d4c1082a92c0a8
Victor Romanov <gerter666@hotmail.com>
2023-05-29 15:14:54 +0300

    just some test scripts

--- scripts/RMG/advmap/Abilities/Test/script.lua
+++ scripts/RMG/advmap/Abilities/Test/script.lua
@@ -1,5 +1,15 @@
 AddHeroEvent.AddListener("BTD_RMG_ability_test", 
 function(hero)
-    ControlHeroCustomAbility(hero, 5, CUSTOM_ABILITY_ENABLED)
-    ControlHeroCustomAbility(hero, 6, CUSTOM_ABILITY_ENABLED)
+    ControlHeroCustomAbility(hero, 3, CUSTOM_ABILITY_ENABLED)
+    ControlHeroCustomAbility(hero, 4, CUSTOM_ABILITY_ENABLED)
 end)
+
+CustomAbility.callbacks[5] = 
+function(hero)
+    print("custom 5")
+end
+
+CustomAbility.callbacks[6] =
+function(hero)
+    print("custom 6")
+end
