bca74d041d45b1dffdf42fb96fe0fac5265601aa
Victor Romanov <gerter666@hotmail.com>
2023-06-01 00:43:41 +0300

    start armies impoves

--- scripts/RMG/advmap/NHF/Common/ArmyGeneration/script.lua
+++ scripts/RMG/advmap/NHF/Common/ArmyGeneration/script.lua
@@ -36,6 +36,133 @@ Generation =
         [HERO_SPEC_GRIFFINS] =
         function(hero)
             AddHeroCreatures(hero, CREATURE_GRIFFIN, random(2) + 1)
+        end,
+
+        [HERO_SPEC_PEASANTS] =
+        function(hero)
+            for slot = 0, 6 do
+                local creature, count = GetObjectArmySlotCreature(hero, slot)
+                if not (creature == 0 or count == 0) then
+                    if creature == CREATURE_PEASANT then
+                        RemoveHeroCreatures(hero, creature, count, slot)
+                        AddHeroCreatures(hero, CREATURE_ARCHER, 6 + random(3))
+                        break
+                    end
+                end
+            end
+        end,
+
+        [HERO_SPEC_FOOTMEN] = 
+        function(hero)
+            for slot = 0, 6 do
+                local creature, count = GetObjectArmySlotCreature(hero, slot)
+                if not (creature == 0 or count == 0) then
+                    if creature == CREATURE_FOOTMAN then
+                        RemoveHeroCreatures(hero, creature, count, slot)
+                        AddHeroCreatures(hero, CREATURE_ARCHER, 6 + random(3))
+                        break
+                    end
+                end
+            end
+        end,
+
+        [HERO_SPEC_BEAR_RIDERS] = 
+        function(hero)
+            for slot = 0, 6 do
+                local creature, count = GetObjectArmySlotCreature(hero, slot)
+                if not (creature == 0 or count == 0) then
+                    if creature == CREATURE_BEAR_RIDER then
+                        RemoveHeroCreatures(hero, creature, count, slot)
+                        AddHeroCreatures(hero, CREATURE_AXE_FIGHTER, 7 + random(4))
+                        break
+                    end
+                end
+            end
+        end,
+
+        [HERO_SPEC_DEFENDER] = 
+        function(hero)
+            for slot = 0, 6 do
+                local creature, count = GetObjectArmySlotCreature(hero, slot)
+                if not (creature == 0 or count == 0) then
+                    if creature == CREATURE_DEFENDER then
+                        RemoveHeroCreatures(hero, creature, count, slot)
+                        AddHeroCreatures(hero, CREATURE_BEAR_RIDER, 2 + random(2))
+                        break
+                    end
+                end
+            end
+        end,
+
+        [HERO_SPEC_VAMPIRES] =
+        function(hero)
+            AddHeroCreatures(hero, CREATURE_VAMPIRE, 1)
+        end,
+
+        [HERO_SPEC_GRIM_FIGHTER] = 
+        function(hero)
+            for slot = 0, 6 do
+                local creature, count = GetObjectArmySlotCreature(hero, slot)
+                if not (creature == 0 or count == 0) then
+                    if creature == CREATURE_ORC_WARRIOR then
+                        RemoveHeroCreatures(hero, creature, count, slot)
+                        AddHeroCreatures(hero, CREATURE_CENTAUR, 6 + random(3))
+                        break
+                    end
+                end
+            end
+        end,
+
+        [HERO_SPEC_GOLEMS] = 
+        function(hero)
+            for slot = 0, 6 do
+                local creature, count = GetObjectArmySlotCreature(hero, slot)
+                if not (creature == 0 or count == 0) then
+                    if creature == CREATURE_IRON_GOLEM then
+                        RemoveHeroCreatures(hero, creature, count, slot)
+                        AddHeroCreatures(hero, CREATURE_GREMLIN, 22 + random(5))
+                        break
+                    end
+                end
+            end
+        end,
+
+        [HERO_SPEC_MAGES] = 
+        function(hero)
+            AddHeroCreatures(hero, CREATURE_MAGI, 1)
+        end,
+
+        [HERO_SPEC_SUCCUBUSES] = 
+        function(hero)
+            AddHeroCreatures(hero, CREATURE_SUCCUBUS, 1)
+        end,
+
+        [HERO_SPEC_HOUNDS] = 
+        function(hero)
+            for slot = 0, 6 do
+                local creature, count = GetObjectArmySlotCreature(hero, slot)
+                if not (creature == 0 or count == 0) then
+                    if creature == CREATURE_HELL_HOUND then
+                        RemoveHeroCreatures(hero, creature, count, slot)
+                        AddHeroCreatures(hero, CREATURE_FAMILIAR, 16 + random(7))
+                        break
+                    end
+                end
+            end
+        end,
+
+        [HERO_SPEC_BLADE_MASTER] = 
+        function(hero)
+            for slot = 0, 6 do
+                local creature, count = GetObjectArmySlotCreature(hero, slot)
+                if not (creature == 0 or count == 0) then
+                    if creature == CREATURE_BLADE_JUGGLER then
+                        RemoveHeroCreatures(hero, creature, count, slot)
+                        AddHeroCreatures(hero, CREATURE_PIXIE, 10 + random(4))
+                        break
+                    end
+                end
+            end
         end
     },
     
@@ -43,12 +170,12 @@ Generation =
     {
         ["Wulfstan"] =
         function(hero)
-            AddHeroCreatures(hero, CREATURE_BROWLER, 2)
+            AddHeroCreatures(hero, CREATURE_BROWLER, 3)
         end,
         
         ["Kujin"] =
         function(hero)
-            AddHeroCreatures(hero, CREATURE_SHAMAN, 1)
+            AddHeroCreatures(hero, CREATURE_SHAMAN, 2)
         end,
     },
 
