4f3e00abae6acf63944b10a2edb60bfca22b1012
Victor Romanov <gerter666@hotmail.com>
2023-06-13 20:11:23 +0300

    start armies improves

--- scripts/RMG/advmap/NHF/Common/ArmyGeneration/script.lua
+++ scripts/RMG/advmap/NHF/Common/ArmyGeneration/script.lua
@@ -3,179 +3,107 @@ Generation =
     BySpec =
     {
         [HERO_SPEC_RADIANT] = 
-        function(hero)
-            AddHeroCreatures(hero, CREATURE_GENIE, 1)
+        function(object)
+            AddObjectCreatures(object, CREATURE_GENIE, 1)
         end,
 
         [HERO_SPEC_MATRON_SALVO] = 
-        function(hero)
-            AddHeroCreatures(hero, CREATURE_MATRON, 1)
+        function(object)
+            AddObjectCreatures(object, CREATURE_MATRON, 1)
         end,
 
         [HERO_SPEC_RIDERS] = 
-        function(hero)
-            AddHeroCreatures(hero, CREATURE_RIDER, 1)
+        function(object)
+            AddObjectCreatures(object, CREATURE_RIDER, 1)
         end,
         
         [HERO_SPEC_WYVERN_TAMER] =
-        function(hero)
-            AddHeroCreatures(hero, CREATURE_WYVERN, 1)
+        function(object)
+            AddObjectCreatures(object, CREATURE_WYVERN, 1)
         end,
 
         [HERO_SPEC_MASTER_OF_ELEMENTS] = 
-        function(hero)
-            AddHeroCreatures(hero, Random.FromSelection(CREATURE_AIR_ELEMENTAL, CREATURE_EARTH_ELEMENTAL, CREATURE_WATER_ELEMENTAL, CREATURE_FIRE_ELEMENTAL), random(2) + 1)
+        function(object)
+            AddObjectCreatures(object, Random.FromSelection(CREATURE_AIR_ELEMENTAL, CREATURE_EARTH_ELEMENTAL, CREATURE_WATER_ELEMENTAL, CREATURE_FIRE_ELEMENTAL), random(2) + 1)
         end,
 
         [HERO_SPEC_SOULHUNTER] = 
-        function(hero)
-            RemoveHeroCreatures(hero, CREATURE_WALKING_DEAD, 9999)
-            AddHeroCreatures(hero, CREATURE_MANES, 7 + random(2))
+        function(object)
+            RemoveObjectCreatures(object, CREATURE_WALKING_DEAD, 9999)
+            AddObjectCreatures(object, CREATURE_MANES, 7 + random(2))
         end,
         
         [HERO_SPEC_GRIFFINS] =
-        function(hero)
-            AddHeroCreatures(hero, CREATURE_GRIFFIN, random(2) + 1)
+        function(object)
+            AddObjectCreatures(object, CREATURE_GRIFFIN, random(2) + 1)
         end,
 
         [HERO_SPEC_PEASANTS] =
-        function(hero)
-            for slot = 0, 6 do
-                local creature, count = GetObjectArmySlotCreature(hero, slot)
-                if not (creature == 0 or count == 0) then
-                    if creature == CREATURE_PEASANT then
-                        RemoveHeroCreatures(hero, creature, count, slot)
-                        AddHeroCreatures(hero, CREATURE_ARCHER, 6 + random(3))
-                        break
-                    end
-                end
-            end
+        function(object)
+            AddObjectCreatures(object, CREATURE_ARCHER, 6 + random(3))
         end,
 
         [HERO_SPEC_FOOTMEN] = 
-        function(hero)
-            for slot = 0, 6 do
-                local creature, count = GetObjectArmySlotCreature(hero, slot)
-                if not (creature == 0 or count == 0) then
-                    if creature == CREATURE_FOOTMAN then
-                        RemoveHeroCreatures(hero, creature, count, slot)
-                        AddHeroCreatures(hero, CREATURE_ARCHER, 6 + random(3))
-                        break
-                    end
-                end
-            end
+        function(object)
+            AddObjectCreatures(object, CREATURE_ARCHER, 6 + random(3))
         end,
 
         [HERO_SPEC_BEAR_RIDERS] = 
-        function(hero)
-            for slot = 0, 6 do
-                local creature, count = GetObjectArmySlotCreature(hero, slot)
-                if not (creature == 0 or count == 0) then
-                    if creature == CREATURE_BEAR_RIDER then
-                        RemoveHeroCreatures(hero, creature, count, slot)
-                        AddHeroCreatures(hero, CREATURE_AXE_FIGHTER, 7 + random(4))
-                        break
-                    end
-                end
-            end
+        function(object)
+            AddObjectCreatures(object, CREATURE_AXE_FIGHTER, 7 + random(4))
         end,
 
         [HERO_SPEC_DEFENDER] = 
-        function(hero)
-            for slot = 0, 6 do
-                local creature, count = GetObjectArmySlotCreature(hero, slot)
-                if not (creature == 0 or count == 0) then
-                    if creature == CREATURE_DEFENDER then
-                        RemoveHeroCreatures(hero, creature, count, slot)
-                        AddHeroCreatures(hero, CREATURE_BEAR_RIDER, 2 + random(2))
-                        break
-                    end
-                end
-            end
+        function(object)
+            AddObjectCreatures(object, CREATURE_BEAR_RIDER, 2 + random(2))
         end,
 
         [HERO_SPEC_VAMPIRES] =
-        function(hero)
-            AddHeroCreatures(hero, CREATURE_VAMPIRE, 1)
+        function(object)
+            AddObjectCreatures(object, CREATURE_VAMPIRE, 1)
         end,
 
         [HERO_SPEC_GRIM_FIGHTER] = 
-        function(hero)
-            for slot = 0, 6 do
-                local creature, count = GetObjectArmySlotCreature(hero, slot)
-                if not (creature == 0 or count == 0) then
-                    if creature == CREATURE_ORC_WARRIOR then
-                        RemoveHeroCreatures(hero, creature, count, slot)
-                        AddHeroCreatures(hero, CREATURE_CENTAUR, 6 + random(3))
-                        break
-                    end
-                end
-            end
+        function(object)
+            AddObjectCreatures(object, CREATURE_CENTAUR, 6 + random(3))
         end,
 
         [HERO_SPEC_GOLEMS] = 
-        function(hero)
-            for slot = 0, 6 do
-                local creature, count = GetObjectArmySlotCreature(hero, slot)
-                if not (creature == 0 or count == 0) then
-                    if creature == CREATURE_IRON_GOLEM then
-                        RemoveHeroCreatures(hero, creature, count, slot)
-                        AddHeroCreatures(hero, CREATURE_GREMLIN, 22 + random(5))
-                        break
-                    end
-                end
-            end
+        function(object)
+            AddObjectCreatures(object, CREATURE_GREMLIN, 22 + random(5))
         end,
 
         [HERO_SPEC_MAGES] = 
-        function(hero)
-            AddHeroCreatures(hero, CREATURE_MAGI, 1)
+        function(object)
+            AddObjectCreatures(object, CREATURE_MAGI, 1)
         end,
 
         [HERO_SPEC_SUCCUBUSES] = 
-        function(hero)
-            AddHeroCreatures(hero, CREATURE_SUCCUBUS, 1)
+        function(object)
+            AddObjectCreatures(object, CREATURE_SUCCUBUS, 1)
         end,
 
         [HERO_SPEC_HOUNDS] = 
-        function(hero)
-            for slot = 0, 6 do
-                local creature, count = GetObjectArmySlotCreature(hero, slot)
-                if not (creature == 0 or count == 0) then
-                    if creature == CREATURE_HELL_HOUND then
-                        RemoveHeroCreatures(hero, creature, count, slot)
-                        AddHeroCreatures(hero, CREATURE_FAMILIAR, 16 + random(7))
-                        break
-                    end
-                end
-            end
+        function(object)
+            AddObjectCreatures(object, CREATURE_FAMILIAR, 16 + random(7))
         end,
 
         [HERO_SPEC_BLADE_MASTER] = 
-        function(hero)
-            for slot = 0, 6 do
-                local creature, count = GetObjectArmySlotCreature(hero, slot)
-                if not (creature == 0 or count == 0) then
-                    if creature == CREATURE_BLADE_JUGGLER then
-                        RemoveHeroCreatures(hero, creature, count, slot)
-                        AddHeroCreatures(hero, CREATURE_PIXIE, 10 + random(4))
-                        break
-                    end
-                end
-            end
+        function(object)
+            AddObjectCreatures(object, CREATURE_PIXIE, 10 + random(4))
         end
     },
     
     ByHero =
     {
         ["Wulfstan"] =
-        function(hero)
-            AddHeroCreatures(hero, CREATURE_BROWLER, 3)
+        function(object)
+            AddObjectCreatures(object, CREATURE_BROWLER, 3)
         end,
         
         ["Kujin"] =
-        function(hero)
-            AddHeroCreatures(hero, CREATURE_SHAMAN, 2)
+        function(object)
+            AddObjectCreatures(object, CREATURE_SHAMAN, 2)
         end,
     },
 
@@ -186,14 +114,16 @@ Generation =
             local hero_spec = Hero.Params.Spec(hero)
             for spec, func in Generation.BySpec do
                 if spec == hero_spec then
-                    startThread(Generation.BySpec[spec], hero)
+                    local object = GetHeroTown(hero) or hero
+                    startThread(Generation.BySpec[spec], object)
                     break
                 end
             end
             --
             for _hero, func in Generation.ByHero do
                 if hero == _hero then
-                    startThread(Generation.ByHero[_hero], hero)
+                    local object = GetHeroTown(hero) or hero
+                    startThread(Generation.ByHero[_hero], object)
                 end
             end
         end)
