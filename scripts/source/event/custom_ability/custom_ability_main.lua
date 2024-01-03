ad9e38e22b607c656fc41de21856dd173cf6e672
Victor Romanov <gerter666@hotmail.com>
2024-01-03 22:07:11 +0300

    #17 main custom ability logic impl

--- scripts/source/event/custom_ability/custom_ability_main.lua
+++ scripts/source/event/custom_ability/custom_ability_main.lua
@@ -1,6 +1,16 @@
+CUSTOM_ABILITY_ARTIFACT = 1
+
 CustomAbility =
 {
     callbacks = {},
+
+    callbacks_for_hero = {
+        [CUSTOM_ABILITY_ARTIFACT] = {}
+    },
+
+    enabled_for_hero = {
+        [CUSTOM_ABILITY_ARTIFACT] = {}
+    },
 
     Init =
     function()
@@ -9,6 +19,12 @@ CustomAbility =
 
     Run = 
     function(hero, ability_id)
-        startThread(CustomAbility.callbacks[ability_id], hero)
+        for callback, active in CustomAbility.callbacks_for_hero[ability_id][hero] do
+            local info = CustomAbility.callbacks[callback]
+            if MCCS_QuestionBoxForPlayers(GetObjectOwner(hero), info.question) then
+                startThread(info.func, hero)
+                break
+            end
+        end
     end
 }
