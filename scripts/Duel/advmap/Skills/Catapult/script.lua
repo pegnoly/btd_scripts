0ab5fac5b842fb12c3fe5f77027c8b622a6019f2
Victor Romanov <gerter666@hotmail.com>
2024-01-09 15:13:44 +0300

    duel: catapult machines give fix

--- scripts/Duel/advmap/Skills/Catapult/script.lua
+++ scripts/Duel/advmap/Skills/Catapult/script.lua
@@ -6,11 +6,11 @@ constructor =
 NewDayEvent.AddListener("BTD_duel_constructor_new_day_event",
 function(day)
     if day == BTD_duel_day_sequence.prefight_day then
-        for hero, alive in AdvMapHeroesInfo.alive_heroes do
-            if hero and alive and HasHeroSkill(hero, PERK_CATAPULT) then
-                for machine = WAR_MACHINE_BALLISTA, WAR_MACHINE_AMMO_CART do
-                    pcall(GiveHeroWarMachine, hero, machine)
-                end
+        for hero, alive_status in AdvMapHeroesInfo.alive_heroes do
+            if hero and alive_status == HERO_CONDITION_ALIVE and HasHeroSkill(hero, PERK_CATAPULT) then
+                --for machine = WAR_MACHINE_BALLISTA, WAR_MACHINE_AMMO_CART do
+                    pcall(GiveHeroWarMachine, hero, WAR_MACHINE_AMMO_CART)
+                --end
             end
         end
     end
