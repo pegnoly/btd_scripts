495a13d9055fb9c1c1225e8dd26ed22b2faac4ce
Victor Romanov <gerter666@hotmail.com>
2024-01-09 15:04:32 +0300

    common: haunt mines reload fix

--- scripts/Common/advmap/Skills/HauntMines/reload.lua
+++ scripts/Common/advmap/Skills/HauntMines/reload.lua
@@ -1,1 +1,1 @@
-consoleCmd("CombatConnection.combat_scripts_paths['haunt_mines_common'] = '/scripts/Common/combat/Skills/HauntMines/script.lua'")
+consoleCmd("@CombatConnection.combat_scripts_paths['haunt_mines_common'] = '/scripts/Common/combat/Skills/HauntMines/script.lua'")
