b374fd4f315d941b85975f4083e3fcc4f2f39d92
Victor Romanov <gerter666@hotmail.com>
2024-01-09 15:15:31 +0300

    duel: snatch after day 3 fix

--- scripts/Duel/advmap/Skills/Snatch/script.lua
+++ scripts/Duel/advmap/Skills/Snatch/script.lua
@@ -1,9 +1,8 @@
-snatch_duel =
-{
+snatch_duel = {
     gold_bonus = 5000,
     gold_by_player = {},
     art_info_by_hero = {},
-    heroes_active = {},
+    active_for_hero = {},
 }
 
 AddHeroEvent.AddListener("btd_duel_snatch_add_hero_listener",
@@ -17,9 +16,6 @@ function(hero)
         --
         if not IsHeroAlive(%hero) then
             return
-        end
-        if HasHeroSkill(%hero, HERO_SKILL_SNATCH) then
-            Resource.Change(%hero, GOLD, snatch_duel.gold_bonus)
         end
         local player = GetObjectOwner(%hero)
         snatch_duel.gold_by_player[player] = GetPlayerResource(player, GOLD)
@@ -42,6 +38,33 @@ function(hero)
             snatch_duel.gold_by_player[player] = GetPlayerResource(player, GOLD)
             for art = 1, 126 do
                 snatch_duel.art_info_by_hero[%hero][art] = GetHeroArtifactsCount(%hero, art)
+            end
+            sleep()
+        end
+    end)
+end)
+
+AddHeroEvent.AddListener("btd_duel_snatch_add_hero_gold_listener",
+function(hero)
+    snatch_duel.active_for_hero[hero] = nil
+    startThread(
+    function()
+        while GetDate(DAY) ~= BTD_duel_day_sequence.freeroam_day do
+            sleep()
+        end
+        while 1 do
+            if IsHeroAlive(%hero) then
+                if not snatch_duel.active_for_hero[%hero] then
+                    if HasHeroSkill(%hero, HERO_SKILL_SNATCH) then
+                        snatch_duel.active_for_hero[%hero] = 1
+                        Resource.Change(%hero, GOLD, snatch_duel.gold_bonus)
+                        return
+                    end
+                else
+                    if not HasHeroSkill(%hero, HERO_SKILL_SNATCH) then
+                        snatch_duel.active_for_hero[%hero] = nil
+                    end
+                end
             end
             sleep()
         end
